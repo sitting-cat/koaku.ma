4b4836aaf3ac565cb372ebf3dfa5b1eb6299b18b
NUCO <93378833+sitting-cat@users.noreply.github.com>
2023-05-25 17:52:23 +0900

    first deploy

---
+++ public/js/hashGenerator.js
@@ -0,0 +1,47 @@
+function hash22(str) {
+    let hash = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        hash = (hash + str.charCodeAt(i)) & 0x3fffff;
+    }
+
+    return hash;
+}
+
+function num2str(num) {
+    const charset = "0123456789adefhijmnqrtABCDEFGHJKLMNPQRSTUVWXY~-";
+    let str = "";
+
+    if (num == 0) return 0;
+
+    while (num > 0) {
+        const digit = num % 47;
+        str = charset[digit] + str;
+        num = Math.floor(num / 47);
+    }
+
+    return str;
+}
+
+function replaceDifferentCharacter(str) {
+    // TODO - Find a better way to implement
+    return str
+        .replace(new RegExp("[0o]"), "O")
+        .replace(new RegExp("[il]"), "1")
+        .replace(new RegExp("[zZ]"), "2")
+        .replace("b", "6")
+        .replace("g", "9")
+        .replace("c", "C")
+        .replace("k", "K")
+        .replace("p", "P")
+        .replace("s", "S")
+        .replace("u", "U")
+        .replace("v", "V")
+        .replace("w", "W")
+        .replace("x", "X")
+        .replace("y", "Y")
+}
+
+function getUrlHash(url) {
+    return num2str(hash22(url));
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,86 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const loadEl = document.querySelector('#load');
-    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-    // // The Firebase SDK is initialized and available here!
-    //
-    // firebase.auth().onAuthStateChanged(user => { });
-    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-    // firebase.firestore().doc('/foo/bar').get().then(() => { });
-    // firebase.functions().httpsCallable('yourFunction')().then(() => { });
-    // firebase.messaging().requestPermission().then(() => { });
-    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-    // firebase.analytics(); // call to activate
-    // firebase.analytics().logEvent('tutorial_completed');
-    // firebase.performance(); // call to activate
-    //
-    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+let app
 
+document.addEventListener('DOMContentLoaded', function () {
     try {
-        let app = firebase.app();
-        let features = [
-            'auth',
-            'database',
-            'firestore',
-            'functions',
-            'messaging',
-            'storage',
-            'analytics',
-            'remoteConfig',
-            'performance',
-        ].filter(feature => typeof app[feature] === 'function');
-        loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
+        app = firebase.app();
+        let appCheck = firebase.appCheck();
+        appCheck.activate(
+            new firebase.appCheck.ReCaptchaEnterpriseProvider('6Ld3MTomAAAAAPbzD7ySZnNL1JSLF_Aa9F_F8Cb3'),
+            true
+        );
     } catch (e) {
+        showError("データベースエラー（使用できません）");
         console.error(e);
-        loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+    } finally {
+        setTimeout(function () { jumpToWebsite(); });
     }
 });
+
+function jumpToWebsite() {
+    const urlobject = new URL(window.location);
+    const target = replaceDifferentCharacter(
+        encodeURIComponent(urlobject.pathname.substring(1))
+    );
+    if (target) {
+        const dbRef = app.database().ref();
+        dbRef.child("short/" + target).once("value").then((snapshot) => {
+            if (snapshot.exists()) {
+                window.location.replace(snapshot.val());
+            } else {
+                showError("転送先が見つかりませんでした");
+            }
+        }).catch((error) => {
+            showError("転送中にエラーが発生しました");
+            console.error(error);
+        })
+    } else {
+        document.getElementById("submit").addEventListener("click", startShorteningUrl, false);
+        document.getElementById("submit").removeAttribute("disabled");
+    }
+}
+
+function startShorteningUrl() {
+    let urlBoxElement = document.getElementById("urlBox");
+    let result = "";
+    if (urlBoxElement.value) {
+        if (isValidURL(urlBoxElement.value)) {
+            document.getElementById("submit").setAttribute("disabled", null);
+            searchGoodHashAndOutputResult(urlBoxElement.value);
+            result = "https://koaku.ma/" + getUrlHash(urlBoxElement.value);
+            document.getElementById("result").value = result;
+            document.getElementById("submit").removeAttribute("disabled");
+            document.getElementById("result").focus();
+        }
+        else {
+            showError("URL以外は短縮できません");
+        }
+    }
+}
+
+function searchGoodHashAndOutputResult(url, retry = 0) {
+    let hash = getUrlHash(url);
+    if (retry > 0) hash = getUrlHash(url + "猫".repeat(retry));
+    const target = app.database().ref("short/" + hash);
+    target.once("value").then((snapshot) => {
+        if (snapshot.exists() && snapshot.val() != url) {
+            searchGoodHashAndOutputResult(url, retry + 1);
+        } else {
+            target.set(url)
+            outputResult("https://koaku.ma/" + hash);
+        }
+    }).catch((error) => {
+        showError("短縮中にエラーが発生しました");
+        console.error(error);
+    })
+}
+
+function outputResult(result) {
+    document.getElementById("result").value = result;
+    document.getElementById("submit").removeAttribute("disabled");
+    document.getElementById("result").focus();
+}
+
+function showError(message) {
+    document.getElementById("error").removeAttribute("style");
+    document.getElementById("error").textContent = message;
+}
