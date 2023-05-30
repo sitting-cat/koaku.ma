a0594db6b740fd4b02e37ba0801f1bf0cb286dac
NUCO <93378833+sitting-cat@users.noreply.github.com>
2023-05-30 16:14:33 +0900

    Add some events for analytics

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
+/*
+
+セッションの開始：ウェブサイトへのアクセス
+商品を表示： F - ターゲットキーが指定されている / S - ウェブサイトコンテンツの表示
+カートに追加: F - 有効なターゲットキー（ここでバリアント指定） / S - フォームへのフォーカス
+決済開始： F - スナップショットの返答 / S - ボタン押下
+購入: F - 転送実施 / S - URL発行
+
+*/
+
 let app
 const urlPrefix = "https://koaku.ma/";
+let koakumaItems = {
+    forward: { item_name: "Koaku.ma - Forwarding Service", item_variant: null },
+    shorten: { item_name: "Koaku.ma - shortened URL" }
+}
+const transaction = generateUuid();
 
 document.addEventListener('DOMContentLoaded', function () {
     try {
@@ -17,26 +32,27 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+document.getElementById("urlBox").addEventListener("focus", urlBoxFocused);
+
+function urlBoxFocused() {
+    sendAddCartEvent(koakumaItems[shorten]);
+    document.getElementById("urlBox").removeEventListener("focus", urlBoxFocused);
+}
+
 function jumpToWebsite() {
     const urlobject = new URL(window.location);
     const target = replaceDifferentCharacter(
         encodeURIComponent(urlobject.pathname.substring(1))
     );
+    if (target) { sendViewEvent(koakumaItems[forward]); }
     if (target && isValidKey(target)) {
+        koakumaItems[forward][item_variant] = target;
+        sendAddCartEvent(koakumaItems[forward]);
         const dbRef = app.database().ref();
         dbRef.child("short/" + target).once("value").then((snapshot) => {
+            sendStartPaymentEvent(koakumaItems[forward]);
             if (snapshot.exists()) {
-                gtag("event", "purchase", {
-                    transaction_id: generateUuid(),
-                    value: 0,
-                    currency: "JPY",
-                    items: [
-                        {
-                            item_name: "Koaku.ma - Forwarding Service",
-                            item_variant: target,
-                        },
-                    ]
-                });
+                sendPurchaseEvent(koakumaItems[forward]);
                 window.location.replace(snapshot.val());
             } else {
                 showError("転送先が見つかりませんでした");
@@ -57,17 +73,15 @@ function jumpToWebsite() {
 function startShorteningUrl() {
     let urlBoxElement = document.getElementById("urlBox");
     let result = "";
+    sendStartPaymentEvent(koakumaItems[shorten]);
     if (urlBoxElement.value) {
         if (isValidURL(urlBoxElement.value)) {
             document.getElementById("submit").setAttribute("disabled", null);
             searchGoodHashAndOutputResult(urlBoxElement.value);
-            result = urlPrefix + getUrlHash(urlBoxElement.value);
-            document.getElementById("result").value = result;
-            document.getElementById("submit").removeAttribute("disabled");
-            document.getElementById("result").focus();
         }
         else {
             showError("URL以外は短縮できません");
+            document.getElementById("submit").removeAttribute("disabled");
         }
     }
 }
@@ -90,17 +104,7 @@ function searchGoodHashAndOutputResult(url, retry = 0) {
 }
 
 function outputResult(result) {
-    gtag("event", "purchase", {
-        transaction_id: generateUuid(),
-        value: 0,
-        currency: "JPY",
-        items: [
-            {
-                item_name: "Koaku.ma - shortened URL",
-                item_variant: result.replace(urlPrefix, ""),
-            },
-        ]
-    });
+    sendPurchaseEvent(koakumaItems[shorten]);
     document.getElementById("result").value = result;
     document.getElementById("submit").removeAttribute("disabled");
     document.getElementById("result").focus();
@@ -120,4 +124,38 @@ function openShortUrl() {
 
 function showContent() {
     document.body.classList.remove("loading");
+    sendViewEvent(koakumaItems[shorten]);
+}
+
+function sendViewEvent(item) {
+    gtag("event", "view_item", {
+        currency: "JPY",
+        value: 0,
+        items: [item]
+    });
+}
+
+function sendAddCartEvent(item) {
+    gtag("event", "add_to_cart", {
+        currency: "JPY",
+        value: 0,
+        items: [item]
+    });
+}
+
+function sendStartPaymentEvent(item) {
+    gtag("event", "begin_checkout", {
+        currency: "JPY",
+        value: 0,
+        items: [item]
+    });
+}
+
+function sendPurchaseEvent(item) {
+    gtag("event", "purchase", {
+        transaction_id: transaction,
+        value: 0,
+        currency: "JPY",
+        items: [item]
+    });
 }
