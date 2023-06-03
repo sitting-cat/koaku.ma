5e5e9237764e0e6501a950b12821f0a472fcd501
NUCO <93378833+sitting-cat@users.noreply.github.com>
2023-06-04 01:06:45 +0900

    「技術的仕様」ページを追加

--- public/js/hashGenerator.js
+++ public/js/hashGenerator.js
@@ -27,7 +27,7 @@ function replaceDifferentCharacter(str) {
     // TODO - Find a better way to implement
     return str
         .replace(new RegExp("[0o]"), "O")
-        .replace(new RegExp("[il]"), "1")
+        .replace(new RegExp("[Il]"), "1")
         .replace(new RegExp("[zZ]"), "2")
         .replace("b", "6")
         .replace("g", "9")
